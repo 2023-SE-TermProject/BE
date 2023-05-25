79d14d370cb59916d67dee41cff227563e8c4c9d
seock1000 <tjrcjs0705@gmail.com>
2023-05-25 14:55:53 +0900

    33

--- src/main/java/com/example/setermproject/domain/seat/dto/reqeust/SeatReservation.java
+++ src/main/java/com/example/setermproject/domain/seat/dto/reqeust/SeatReservation.java
@@ -1,10 +1,14 @@
 package com.example.setermproject.domain.seat.dto.reqeust;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class SeatReservation {
     private Long seatId;
     private Long memberId;
